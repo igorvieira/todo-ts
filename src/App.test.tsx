436850cd54dbb8740df61234436a7381312dc0e4
Igor Vieira <igor.p.r.vieira@gmail.com>
2021-12-31 10:47:44 -0300

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render a title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hey/i);
   expect(linkElement).toBeInTheDocument();
 });
