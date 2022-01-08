62fb03faebab65098528bf1d5004bd18766b8933
Igor Vieira <igor.p.r.vieira@gmail.com>
2022-01-08 16:55:23 -0300

    add function for concat new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  interface Task  {
+    id: string;
+    title: string;
+  }
+
+  const initialContent = { id: '', title: ''}
+
+
+  const [ newItem, setNewItem ] = useState<Task>(initialContent)
+  const [ listOfItems, setListOfItems ] = useState<Task[]>([]);
+
+  const handleAddNewItems = (): void => {
+    setListOfItems([...listOfItems, newItem])
+  }
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+
+    setNewItem({
+      id: `${event.target.value} - ${Math.random() * 100}`,
+      title: event.target.value
+    })
+  }
+
   return (
     <div>
-      Hey
+      <div>
+        {
+        listOfItems.map((item: Task) => (
+            <div>
+              {item?.title}
+            </div>
+          ))
+        }
+      </div>
+
+      <div>
+        <input
+          value={newItem?.title}
+          onChange={(e) => handleOnChange(e)}
+        />
+        <button onClick={() => handleAddNewItems()}>Add</button>
+      </div>
     </div>
   );
 }
